92e14e52e98b9a283dc44db6c38802cf129e6a3b
eazyciphers <69615593+eazyciphers@users.noreply.github.com>
2020-08-13 13:19:12 +0530

    Update code.py

--- Character recognition in image/code.py
+++ Character recognition in image/code.py
@@ -3,37 +3,37 @@ import numpy as np
 import pytesseract
 from PIL import Image
 
-# Path of working folder on Disk
-src_path = r'C:\Users\dell\Desktop\Image to text\pic.jpeg'
+# path of pytesseract execution folder 
+pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
 
-def get_string(img_path):
-    # Read image with opencv
-    img = cv2.imread(img_path)
+# Path of image
+main_path = r'C:\Users\dell\Desktop\Image to text\qu12.png'
 
-    # Convert to gray
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def get_string(pic_path):
+    # Reading picture with opencv
+    pic = cv2.imread(pic_path)
 
-    # Apply dilation and erosion to remove some noise
+    # grey-scale the picture
+    pic = cv2.cvtColor(pic, cv2.COLOR_BGR2GRAY)
+
+    # Do dilation and erosion to eliminate unwanted noises
     kernel = np.ones((1, 1), np.uint8)
-    img = cv2.dilate(img, kernel, iterations=1)
-    img = cv2.erode(img, kernel, iterations=1)
+    pic = cv2.dilate(pic, kernel, iterations=20)
+    pic = cv2.erode(pic, kernel, iterations=20)
 
     # Write image after removed noise
-    cv2.imwrite(src_path + "removed_noise.png", img)
+    cv2.imwrite(main_path + "no_noise.png", pic)
 
-    #  Apply threshold to get image with only black and white
-    img = cv2.adaptiveThreshold(img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
+    #  threshold applying to get only black and white picture 
+    pic = cv2.adaptiveThreshold(pic, 300, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 31, 2)
 
-    # Write the image after apply opencv to do some ...
-    cv2.imwrite(src_path + "thres.png", img)
+    # Write the image for later recognition process 
+    cv2.imwrite(main_path + "threshold.png", pic)
 
-    # Recognize text with tesseract for python
-    result = pytesseract.image_to_string(Image.open(src_path + "thres.png"))
+    # Character recognition with tesseract
+    final = pytesseract.image_to_string(Image.open(main_path + "threshold.png"))
 
-    # Remove template file
-    #os.remove(temp)
-
-    return result
+    return final
 
 
 #starts recognition of characters
