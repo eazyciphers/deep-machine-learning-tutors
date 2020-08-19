aedd9fbf0d85c2f6cf2e2ddc5f80282057e40e0c
eazyciphers <69615593+eazyciphers@users.noreply.github.com>
2020-08-19 16:09:21 +0530

    Update code.py

--- Real-Time Face Recognition/code.py
+++ Real-Time Face Recognition/code.py
@@ -28,14 +28,16 @@ while True:
     ret, frame = video_capture.read()
     # Change the image from BGR color (which OpenCV uses) to RGB color (which face_recognition uses)
     rgb_frame = frame[:, :, ::-1]
+    
     # Find all the faces and face enqcodings in the frame of video
     face_locations = face_recognition.face_locations(rgb_frame)
     face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    
     # Loop through each face in this frame of video
     for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
-        name = "Unknown"
+        name = "Not Known Still In Recognizing State"
         # If a match was found in known_face_encodings, just use the first one.
         # if True in matches:
         #     first_match_index = matches.index(True)
@@ -53,9 +55,11 @@ while True:
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
     # Display the resulting image
     cv2.imshow('Video', frame)
+    
     # Hit 'q' on the keyboard to quit!
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+        
 # Release handle to the webcam
 video_capture.release()
 cv2.destroyAllWindows()
